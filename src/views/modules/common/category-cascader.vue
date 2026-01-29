5b4d9c9743094f9a65d354d92695f6e7938777e7
HuachengXu <xuhuac@foxmail.com>
2026-01-30 01:09:55 +0800

    1.商品品牌和分类级联更新

--- src/views/modules/common/category-cascader.vue
+++ src/views/modules/common/category-cascader.vue
@@ -53,8 +53,8 @@ export default {
     },
     paths(v){
       this.$emit("update:catelogPath",v);
-      //还可以使用pubsub-js进行传值
-      this.PubSub.publish("catPath",v);
+      // 还可以使用pubsub-js进行传值
+      // this.$PubSub.publish("catPath",v); //需要安装 pubsub-js 库
     }
   },
   //方法集合
